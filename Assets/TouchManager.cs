0a7f521603f7e65a8c57acefe64b762ffa559bd2
yamazaki0305 <yamazaki0305@hotmail.com>
2018-08-30 00:18:12 +0900

    Pinball　下記間違いを修正　タップ操作追加 ▼間違いを修正 調べたところ「Ball」オブジェクトに「Sphere Collider」が2つアタッチされていました。 また重力の設定が行われていなかったようです。 「6.5 ゲーム中の重力を変更」 ▼タップ操作を追加 TouchManager.cs

---
+++ Assets/TouchManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TouchManager : MonoBehaviour {
+
+    public FripperController LeftFripper;
+    public FripperController RightFripper;
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+    
+    bool touch_now_left;
+    bool touch_now_right;
+
+
+    // Use this for initialization
+    void Start () {
+        // タッチ中
+        touch_now_left = false;
+        touch_now_right = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var touch_id = AppUtil.GetTouch();
+
+        if (touch_id == TouchInfo.Began)
+        {
+            Vector3 touch_pos = AppUtil.GetTouchPosition();
+            Debug.Log("タッチした");
+            if (touch_pos.x < Screen.width / 2)
+            {
+                touch_now_left = true;
+                LeftFripper.SetAngle(flickAngle);
+            }
+            else
+            {
+                touch_now_right = true;
+                RightFripper.SetAngle(flickAngle);
+            }
+
+        }
+        else if (touch_id == TouchInfo.Ended)
+        {
+            Vector3 touch_pos = AppUtil.GetTouchPosition();
+            Debug.Log("タッチ離した");
+            if (touch_now_left)
+                LeftFripper.SetAngle(defaultAngle);
+            else if (touch_now_right)
+                RightFripper.SetAngle(defaultAngle);
+
+            touch_now_left = false;
+            touch_now_right = false;
+        }
+
+    }
+}
